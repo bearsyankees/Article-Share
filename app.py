6b6fdbbb36ea7af39f92ed4bf5678b93a41f119c
bearsyankees <bearsyankees@gmail.com>
2023-02-06 12:16:57 -0500

    new db

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def create_app():
     ))
 
     app.secret_key = environ['APP_SECRET']
-    db_uri = environ.get("DATABASE_URL")  # or other relevant config var
+    db_uri = environ["DATABASE_URL"]  # or other relevant config var
     if db_uri.startswith("postgres://"):
         db_uri = db_uri.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
