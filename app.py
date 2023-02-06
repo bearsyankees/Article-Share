aa2c8344fbeca0b699de5adcf167d237dfa12d0a
bearsyankees <bearsyankees@gmail.com>
2023-02-06 12:13:28 -0500

    new db

--- app.py
+++ app.py
@@ -60,7 +60,6 @@ def create_app():
     db_uri = environ.get("DATABASE_URL")  # or other relevant config var
     if db_uri.startswith("postgres://"):
         db_uri = db_uri.replace("postgres://", "postgresql://", 1)
-
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
